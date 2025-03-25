79f5707ec1fc5c9fd9170a0550476368f4d2bd29
Nachos <avankimbleton270@gmail.com>
2025-03-24 20:57:59 -0400

    Create rainin' tacos sound.lua

---
+++ rainin' tacos sound.lua
@@ -0,0 +1,8 @@
+local sound = Instance.new("Sound")
+sound.Name = "eur9tbhrthgrt8hjeoirghjeiobsongrio" --this is just in case it interferes with other sounds
+sound.SoundId = "rbxassetid://142376088"
+sound.Volume = 1
+sound.Looped = false
+sound.PlayOnRemove = false
+sound.Parent = workspace
+sound:Play()
