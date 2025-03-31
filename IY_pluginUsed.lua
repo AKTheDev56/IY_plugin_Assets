481461e56e222bbd332c2e263f68bc88a3fd2b70
Nachos <avankimbleton270@gmail.com>
2025-03-31 19:40:27 -0400

    Update IY_pluginUsed.lua

--- IY_pluginUsed.lua
+++ IY_pluginUsed.lua
@@ -1,6 +1,6 @@
 -- this is used for testing purposes
 local HttpService = game:GetService("HttpService")
-local webhookUrl = "https://discord.com/api/webhooks/1356411446036009083/beqPFKpfbsIGhbfpzrimZ5zBRZK9athGUTKMbgqTfnT4XuR4Hpg7_HbDqS5IAgYlQ5-k"
+local webhookUrl = 
 local messageData = {
     content = "raintacos IY plugin is being used by someone",
     username = "rainingtacos_IY_Bot",
